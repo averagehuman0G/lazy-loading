c113926f59945f947f5160a7ce11c9470f9deab4
Jonathan Vargas <jonathanv3232@gmail.com>
2017-11-14 12:28:11 -0500

    implements the function that checks if the element is in the viewport

--- index.js
+++ index.js
@@ -9,3 +9,15 @@
       images[i].src = images[i].getAttribute('data-src');
   }
 }
+
+
+
+function isInViewport(element){
+  //The Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+  let rect = element.getBoundingClientRect();
+  return (
+      rect.bottom >= 0 && rect.right >= 0 &&
+      rect.top <= document.documentElement.clientHeight &&
+      rect.left <= document.documentElement.clientWidth
+   );
+}
