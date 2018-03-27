8063fb586bce783830180896007015941dbc9227
Jonathan Vargas <jonathanv3232@gmail.com>
2018-03-27 12:19:07 -0400

    refactors and adds a debounce function for the scroll listener

--- index.js
+++ index.js
@@ -1,35 +1,55 @@
-let images, imagesArr;
-function setUp() {
+function beginToLazyLoad() {
+  //since we only onload once go ahead and remove the listener
+  window.onload = null;
+
+  let images, imagesArr;
+
   //grabs all images and stores them in a nodes list
   images = document.getElementsByClassName('lazy');
   imagesArr = Array.from(images);
-  lazyLoad()
-}
 
-function lazyLoad() {
-  //iterates through the node list
-  for(let i = 0; i < imagesArr.length; i++){
-    //checks if it is in view, in order to load
-    if( isInViewport(imagesArr[i]) ) {
-      imagesArr[i].src = imagesArr[i].getAttribute('data-src');
-      imagesArr.splice(i, 1);
-      console.log(imagesArr.length);
+  function lazyLoad() {
+    //iterates through the node list
+    for (let i = 0; i < imagesArr.length; i++) {
+      //checks if it is in view, in order to load
+      if (isInViewport(imagesArr[i])) {
+        imagesArr[i].src = imagesArr[i].getAttribute('data-src');
+        imagesArr.splice(i, 1);
+        console.log(imagesArr.length);
+      }
     }
   }
-}
 
-
-
-function isInViewport(element){
-  //The Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-  let rect = element.getBoundingClientRect();
-  return (
-      rect.bottom >= 0 && rect.right >= 0 &&
+  function isInViewport(element) {
+    //The Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+    let rect = element.getBoundingClientRect();
+    return (
+      rect.bottom >= 0 &&
+      rect.right >= 0 &&
       rect.top <= document.documentElement.clientHeight &&
       rect.left <= document.documentElement.clientWidth
-   );
+    );
+  }
+
+  function debounce(func, wait, immediate) {
+    let timeout;
+    return function() {
+      let context = this,
+        args = arguments;
+      let later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      let callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+
+  window.addEventListener('scroll', debounce(lazyLoad, 100));
+
+  lazyLoad();
 }
 
-window.onload = setUp;
-
-window.addEventListener('scroll', lazyLoad);
+window.onload = beginToLazyLoad;
